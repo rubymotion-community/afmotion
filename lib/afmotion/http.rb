16be6009d6043c1c8e2b0b8371489d50d39ce4e1
Clay Allsopp <clay.allsopp@gmail.com>
2013-10-15 18:22:14 -0700

    fix the short-hands

--- lib/afmotion/http.rb
+++ lib/afmotion/http.rb
@@ -1,22 +1,6 @@
 module AFMotion
-  module HTTPBuilder
-    def self.included(base)
-      AFMotion::HTTP_METHODS.each do |method|
-        base.send(:define_singleton_method, method, -> (url, parameters = {}, &callback) do
-          operation_manager.send(method.to_s.upcase, url,
-            parameters: parameters,
-            success: AFMotion::Operation.success_block_for_http_method(method, callback),
-            failure: AFMotion::Operation.failure_block(callback))
-        end)
-      end
-    end
-  end
-
-  module HTTP
-    include AFMotion::HTTPBuilder
-
-    module_function
-    def operation_manager
+  class HTTP
+    def self.operation_manager
       @operation_manager ||= begin
         manager = AFHTTPRequestOperationManager.manager
         configure_manager(manager)
@@ -24,43 +8,43 @@ module AFMotion
       end
     end
 
-    def configure_manager(manager)
+    def self.configure_manager(manager)
     end
   end
 
-  module JSON
-    include AFMotion::HTTPBuilder
-
-    module_function
-    def configure_manager(manager)
+  class JSON < HTTP
+    def self.configure_manager(manager)
       manager.json!
     end
   end
 
-  module XML
-    include AFMotion::HTTPBuilder
-
-    module_function
-    def configure_manager(manager)
+  class XML < HTTP
+    def self.configure_manager(manager)
       manager.xml!
     end
   end
 
-  module PLIST
-    include AFMotion::HTTPBuilder
-
-    module_function
-    def configure_manager(manager)
+  class PLIST < HTTP
+    def self.configure_manager(manager)
       manager.plist!
     end
   end
 
-  module Image
-    include AFMotion::HTTPBuilder
-
-    module_function
-    def configure_operation(operation)
+  class Image < HTTP
+    def self.configure_manager(operation)
       operation.image!
+    end
+  end
+
+  [HTTP, JSON, XML, PLIST, Image].each do |base|
+    AFMotion::HTTP_METHODS.each do |method_name|
+      method_signature = "#{method_name.to_s.upcase}:parameters:success:failure:"
+      base.define_singleton_method(method_name, -> (url, parameters = {}, &callback) do
+        base.operation_manager.send(method_signature, url,
+          parameters,
+          AFMotion::Operation.success_block_for_http_method(method_name, callback),
+          AFMotion::Operation.failure_block(callback))
+      end)
     end
   end
 end
