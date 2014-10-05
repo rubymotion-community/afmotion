c2a1d95bf4e279d34422469a0dc2e22503714dfe
Chris Radford <chris@chrisradford.com>
2014-10-05 21:14:03 +0100

    Add nil check for #status_code
    Added a fix for `#status_code` when `self.operation` is not set.
    
    Resolves #66

--- lib/afmotion/http_result.rb
+++ lib/afmotion/http_result.rb
@@ -23,9 +23,9 @@ module AFMotion
     end
 
     def status_code
-      if self.operation
+      if self.operation && self.operation.response
         self.operation.response.statusCode
-      else
+      elsif self.task
         self.task.response.statusCode
       end
     end
